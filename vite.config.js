7f242119fa211364884721889a783b5ea96657eb
jorabey <sjorabek42@gmail.com>
2026-06-16 08:00:17 -0400

    Simplify vite.config.js by removing PWA setup
    Removed PWA plugin configuration and comments from vite.config.js.

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,7 @@
-// vite.config.js ichida namuna:
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { VitePWA } from 'vite-plugin-pwa';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({
-      registerType: 'autoUpdate', // 🚀 Yangilanishlar avtomatik o'tishi uchun juda muhim!
-      manifest: {
-        // manifest sozlamalari...
-      }
-    })
-  ]
-});
+  plugins: [react()]
+})
